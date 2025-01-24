1da6324c60cd3b3e5d230a5076e7af76b571e9e3
hiigeki <romeorqs@gmail.com>
2025-01-24 08:18:20 +0100

    delai test

--- backnodejs/test.js
+++ backnodejs/test.js
@@ -7,6 +7,12 @@ const req1 = coap.request('coap://localhost/connect/2025');
 console.log("request coap connect OK");
 req1.end();
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sleep(1000);
+
 console.log("request coap publish ...");
 const req2 = coap.request('coap://localhost/2025/normal');
 console.log("request coap publish OK");
